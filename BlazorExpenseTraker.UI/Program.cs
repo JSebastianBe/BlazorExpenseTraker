4be9c21b44394a4370862659030bec564775edd3
JSebastianBe <sebastianbm97@gmail.com>
2024-11-07 11:41:32 -0500

    Creación correcta del proyecto de UI

--- BlazorExpenseTraker.UI/Program.cs
+++ BlazorExpenseTraker.UI/Program.cs
@@ -1,24 +1,27 @@
-using BlazorExpenseTraker.UI.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using BlazorExpenseTraker.UI.Interfaces;
 using BlazorExpenseTraker.UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
 
 builder.Services.AddServerSideBlazor().AddCircuitOptions(options => {options.DetailedErrors = true;});
 
 builder.Services.AddHttpClient<ICategoryService, CategoryService>(
     client => {client.BaseAddress = new Uri("http://localhost:5041");}
 );
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -26,9 +29,10 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
-app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.Run();
