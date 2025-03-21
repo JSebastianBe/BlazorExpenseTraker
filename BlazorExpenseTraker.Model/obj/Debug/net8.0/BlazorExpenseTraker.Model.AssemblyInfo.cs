36d5c3b794373b43f5cd75e3ff3a901f959b1a2d
JSebastianBe <sebastianbm97@gmail.com>
2025-03-21 10:49:15 -0500

    Mejora look and feel general

--- BlazorExpenseTraker.Model/obj/Debug/net8.0/BlazorExpenseTraker.Model.AssemblyInfo.cs
+++ BlazorExpenseTraker.Model/obj/Debug/net8.0/BlazorExpenseTraker.Model.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorExpenseTraker.Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e81c3c4fa4071d1f5c6db3acb90b765070f18ac6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b12b0cb423636edc3859b901a8390c044559e06")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorExpenseTraker.Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorExpenseTraker.Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
